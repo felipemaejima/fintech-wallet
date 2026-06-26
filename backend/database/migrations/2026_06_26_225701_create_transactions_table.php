37cb9d33bc4839e37b88e1e3a88f2b3017f55093
felipemaejima <felipemaejima@gmail.com>
2026-06-26 20:32:40 -0300

    fix (Transaction): Ajuste na definição da tabela (id -> uuid)

--- backend/database/migrations/2026_06_26_225701_create_transactions_table.php
+++ backend/database/migrations/2026_06_26_225701_create_transactions_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
   public function up(): void
   {
     Schema::create('transactions', function (Blueprint $table) {
-      $table->id();
+      $table->uuid('id')->primary();
       $table->foreignId('wallet_id')
         ->constrained()
         ->cascadeOnDelete();
