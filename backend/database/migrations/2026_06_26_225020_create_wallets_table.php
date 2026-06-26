a3dab354bb3f1aa6ba156e83aa020c027222e2d9
felipemaejima <felipemaejima@gmail.com>
2026-06-26 20:07:03 -0300

    style: Ajuste na identeção das migrations das tabelas wallets e transactions

--- backend/database/migrations/2026_06_26_225020_create_wallets_table.php
+++ backend/database/migrations/2026_06_26_225020_create_wallets_table.php
@@ -4,23 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
-    {
-        Schema::create('wallets', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')
-                  ->unique()
-                  ->constrained()
-                  ->cascadeOnDelete();
-            $table->decimal('balance', 15, 2)->default(0);
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+  public function up(): void
+  {
+    Schema::create('wallets', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('user_id')
+        ->unique()
+        ->constrained()
+        ->cascadeOnDelete();
+      $table->decimal('balance', 15, 2)->default(0);
+      $table->timestamps();
+    });
+  }
 
-    public function down(): void
-    {
-        Schema::dropIfExists('wallets');
-    }
+  public function down(): void
+  {
+    Schema::dropIfExists('wallets');
+  }
 };
